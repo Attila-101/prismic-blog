58b9ad2fdfaee4318118686c5e5e338ffc61b8f4
Attila <71675386+Attila-101@users.noreply.github.com>
2022-07-11 01:14:19 +0000

    fix link on contact page

--- .slicemachine/assets/slices/Text/index.stories.js
+++ .slicemachine/assets/slices/Text/index.stories.js
@@ -5,5 +5,8 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text","items":[],"primary":{"text":[{"type":"paragraph","text":"Labore esse nulla ullamco qui. Sunt ullamco ipsum proident sunt irure. Velit exercitation eu elit voluptate sint anim esse cillum veniam elit mollit et labore.","spans":[]}],"link":{"link_type":"Web","url":"https://slicemachine.dev"}},"id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text","items":[],"primary":{"text":[{"type":"paragraph","text":"Enim laboris veniam excepteur adipisicing velit deserunt esse incididunt do elit in officia.","spans":[]}],"link":{"link_type":"Web","url":"http://twitter.com"}},"id":"_Default"}} />
 _Default.storyName = 'Default'
+
+export const _TextWithLink = () => <MyComponent slice={{"variation":"textWithLink","name":"Text With Link","slice_type":"text","items":[],"primary":{"text":[{"type":"paragraph","text":"Exercitation aute ipsum eiusmod aute nostrud. Eu sunt sunt velit veniam duis.","spans":[]}],"link":{"link_type":"Web","url":"http://twitter.com"}},"id":"_TextWithLink"}} />
+_TextWithLink.storyName = 'Text With Link'
--- slices/Text/index.js
+++ slices/Text/index.js
@@ -5,12 +5,16 @@ import { Bounded } from "../../components/Bounded";
 
 const Text = ({ slice }) => {
   return (
-    <Bounded >
-      {prismicH.isFilled.richText(slice.primary.text) && (
+    <Bounded as="section">
+      {prismicH.isFilled.link(slice.primary.link) ? (
         <div className="font-serif leading-relaxed md:text-xl md:leading-relaxed">
-          <PrismicLink  field={slice.primary.link}><PrismicRichText field={slice.primary.text} /></PrismicLink>
+          <PrismicLink field={slice.primary.link}><PrismicRichText field={slice.primary.text} /></PrismicLink>
         </div>
-      )}
+      ): 
+      <div className="font-serif leading-relaxed md:text-xl md:leading-relaxed">
+          <PrismicRichText field={slice.primary.text} />
+        </div>
+      }
     </Bounded>
   );
 };
